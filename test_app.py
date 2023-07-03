f6107279c3e01bfbc8c7c99a0f1bb6500985b4aa
Amarczak001 <marczakadam005@gmail.com>
2023-07-03 15:33:30 +0200

    add tests

---
+++ test_app.py
@@ -0,0 +1,18 @@
+import pytest
+
+from app import app as flask_app
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_hello_world(client):
+    res = client.get('/')
+    assert res.status_code == 200
+    assert res.data == b'Hello, World!'
